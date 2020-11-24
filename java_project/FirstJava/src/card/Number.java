3c6e974b79dbddf7f0bbab3d7171f9436f1a334d
KIM <jnghyn93@gmail.com>
2020-11-24 18:06:56 +0900

    2020.11.24

---
+++ java_project/FirstJava/src/card/Number.java
@@ -0,0 +1,23 @@
+package card;
+
+public class Number {
+	
+	int num;
+	
+	Number(int number) {
+		num = number;
+		System.out.println("생성자가 실행되었습니다.1");
+	}
+	
+	Number(){
+		num=10;
+		System.out.println("생성자가 실행되었습니다.2");
+	}
+	
+	int getNumber() {
+		return num;
+	}
+	
+	
+
+}
