5970a294d5ac556d358671d36c0e9d50ff397731
Jung Hyun Kim <jnghyn93@gmail.com>
2021-01-08 15:54:22 +0900

    20210108

--- spring_project/FirstSpring/src/main/java/com/aia/firstspring/controller/OrderController.java
+++ spring_project/FirstSpring/src/main/java/com/aia/firstspring/controller/OrderController.java
@@ -1,4 +1,4 @@
-package com.aia.firstspring.member.controller;
+package com.aia.firstspring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
---
+++ spring_project/FirstSpring/src/main/java/com/aia/firstspring/controller/SearchController.java
@@ -0,0 +1,35 @@
+package com.aia.firstspring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aia.firstspring.domain.SearchType;
+
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/search/search")
+	public String search() {
+		return "search/search";
+	}
+	
+	@ModelAttribute("searchTypeList")
+	public List<SearchType> getSearchType(){
+		List<SearchType> options = new ArrayList<SearchType>();
+		options.add(new SearchType(1, "팀이름+선수이름"));
+		options.add(new SearchType(1, "팀이름"));
+		options.add(new SearchType(1, "선수이름"));
+		return options;
+		
+	}
+	
+	@ModelAttribute("queryList")
+	public String[] getqueryList() {
+		return new String[] {"손흥민", "매시", "호날두"};
+	}
+
+}
