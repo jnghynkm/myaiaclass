6297fca249ec0dfb557cf3af7d8ac087fbdd4ef4
Jung Hyun Kim <jnghyn93@gmail.com>
2021-01-25 23:30:06 +0900

    2021.01.25

--- spring_project/Spring-OpenProject/src/main/java/com/aia/op/member/service/MemberRegService.java
+++ spring_project/Spring-OpenProject/src/main/java/com/aia/op/member/service/MemberRegService.java
@@ -15,67 +15,66 @@ import com.aia.op.member.domain.MemberRegRequest;
 
 @Service
 public class MemberRegService {
-	
+
 	private MemberDao dao;
-	
+
 	@Autowired
 	private SqlSessionTemplate template;
 	
+	@Autowired
+	private MailSenderService mailSenderService;
+
 	// 파일을 업로드, 데이터베이스 저장
-	public int memberReg(
-			MemberRegRequest regRequest,
-			HttpServletRequest request
-			) {
+	public int memberReg(MemberRegRequest regRequest, HttpServletRequest request) {
 		
 		int result = 0;
 		
 		File newFile = null;
-		
 		String newFileName = null;
 		
 		if(!regRequest.getUserPhoto().isEmpty()) {
-			
 			// 웹 경로
 			String uploadPath = "/fileupload/member";
 			// 시스템의 실제 경로
 			String saveDirPath = request.getSession().getServletContext().getRealPath(uploadPath);
 			// 새로운 파일 이름
-			newFileName = regRequest.getUserid()+System.currentTimeMillis();
+			newFileName = regRequest.getUserid() + System.currentTimeMillis();
 			newFile = new File(saveDirPath, newFileName);
 			
 			/* 파일 저장 */
 			try {
 				regRequest.getUserPhoto().transferTo(newFile);
 			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 		
 		Member member = regRequest.toMember();
-		member.setMemberphoto(newFileName);
-		
+		if(newFileName != null) {
+			member.setMemberphoto(newFileName);
+		}
+
 		try {
 			// 데이터 베이스 입력
 			dao = template.getMapper(MemberDao.class);
 			result = dao.insertMember(member);
+			
+			// 메일발송 : 인증 처리를 하는 페이지 /op/member/verify?id=40&code=난수
+			int mailsendCnt = mailSenderService.send(member);
+			System.out.println("메일 발송 처리 횟수 : " + mailsendCnt);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
-			// 현재 저장한 파일이 있다면? -> 삭제
-			if(newFile != null && newFile.exists()) {
+			// 현재 저장한 파일이 있다면??!! -> 삭제
+			if (newFile != null && newFile.exists()) {
 				newFile.delete();
 			}
-			
 		}
-		
+
 		return result;
 	}
-	
-	
-	
-	
+
 }
